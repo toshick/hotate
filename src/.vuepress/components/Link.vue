e898b4c1c9d6607bc983f8842a8f89a192bdd2cb
toshick <toshickjazz@mac.com>
2022-11-06 22:20:31 +0900

    feat: add favorite, event page

---
+++ src/.vuepress/components/Link.vue
@@ -0,0 +1,24 @@
+<template>
+  <a class="link" v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </a>
+</template>
+
+<style scoped>
+  a.link {
+    --btn-fontsize: 16px;
+    display: inline-block;
+    font-size: var(--btn-fontsize);
+    padding: calc(var(--unit) * 1) calc(var(--unit) * 3);
+    margin: calc(var(--unit) * 1) 0;
+    border-radius: calc(var(--unit) * 1);
+    border: none;
+    background-color: #21a8af;
+    color: white;
+    cursor: pointer;
+  }
+  a.link:hover {
+    text-decoration: none;
+    background-color: #1b8f95;
+  }
+</style>
--- src/index.md
+++ src/index.md
@@ -2,14 +2,15 @@
 home: true
 heroImage: img/chunum002_tatematsu.png
 tagline: 
-actionText: Quick Start →
-actionLink: /guide/
+actionText: Now or Never →
+actionLink: /event/
+
 features:
-- title: Feature 1 Title
-  details: Feature 1 Description
-- title: Feature 2 Title
-  details: Feature 2 Description
-- title: Feature 3 Title
-  details: Feature 3 Description
+- title: すなはち
+  details: 武士道は死狂ひなり。一人の殺害を数十人して仕かぬるもの
+- title: すなはち
+  details: 本気にては大業はならず。気違ひになりて死狂ひするまでなり
+- title: すなはち
+  details: 又武士道は於て分別出来れば、はや後るるなり。
 footer: Made with Anger
 ---
